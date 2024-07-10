da605d19ff050cbc816117f03c221fa5cbb9b394
Krishna Bansal <krishnabansal890@gmail.com>
2024-07-10 15:56:06 +0530

    (Add): tsconfig file and  dev & start script

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import express, { Request, Response } from "express";
 import router from "./routes";
+import dotenv from 'dotenv';
+dotenv.config();
+
 
 const app = express();
 const port = process.env.PORT || 3000;
